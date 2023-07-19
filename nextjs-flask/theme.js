181dc71a647adbf2d15721411d54ed6f4be82cbd
tyfurrier <tyler.furrier@atscale.com>
2023-07-19 14:55:02 +0100

    idk

---
+++ nextjs-flask/theme.js
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({ config });
+
+export default theme;
+
+// testing commit

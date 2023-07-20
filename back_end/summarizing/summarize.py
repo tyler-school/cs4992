51e9ccc1c8a58f6702cd5c764b6c514197d24600
willpreebs <wpriebe16124@gmail.com>
2023-07-20 22:55:37 +0100

    NEWS-9 moved files to back_end/summarizing

--- back_end/summarizing/summarize.py
+++ back_end/summarizing/summarize.py
@@ -1,19 +1,24 @@
 from skllm.config import SKLLMConfig
 from skllm.preprocessing import GPTSummarizer
 
+# WARNING: We have a limited free trial with openAI. Try to limit the use of this function
+
 class Summarizer():
 
-    def summarize(text):
+    def summarize(text, max=15):
 
         key_file = open("newsfeedKey2", 'r')
         SECRET_KEY = key_file.read().rstrip()
         orgkey_file = open("newsfeedOrgKey2", 'r')
         OPENAI_ORG_ID = orgkey_file.read().rstrip()
+
         SKLLMConfig.set_openai_key(SECRET_KEY)
         SKLLMConfig.set_openai_org(OPENAI_ORG_ID)
 
-        gpt_summarizer = GPTSummarizer(openai_model = "gpt-3.5-turbo", max_words = 15)
+        gpt_summarizer = GPTSummarizer(openai_model = "gpt-3.5-turbo", max_words = max)
         summaries = gpt_summarizer.fit_transform(text)
+       
         print(summaries)
+        return summaries
 
 

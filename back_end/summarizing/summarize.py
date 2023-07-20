891666c05f125e19482c4210a16675e43bdae676
willpreebs <wpriebe16124@gmail.com>
2023-07-20 23:59:06 +0100

    NEWS-9 Working summarizer cli with file option

--- back_end/summarizing/summarize.py
+++ back_end/summarizing/summarize.py
@@ -1,3 +1,6 @@
+import sys
+import getopt
+
 from skllm.config import SKLLMConfig
 from skllm.preprocessing import GPTSummarizer
 
@@ -5,20 +8,40 @@ from skllm.preprocessing import GPTSummarizer
 
 class Summarizer():
 
-    def summarize(text, max=15):
+    def __init__(self):
 
-        key_file = open("newsfeedKey2", 'r')
-        SECRET_KEY = key_file.read().rstrip()
-        orgkey_file = open("newsfeedOrgKey2", 'r')
-        OPENAI_ORG_ID = orgkey_file.read().rstrip()
+        # Ask Will for the keys
+        key_file = open("secret_keys/secretKey", 'r')
+        self.SECRET_KEY = key_file.read().rstrip()
+        orgkey_file = open("secret_keys/secretOrgKey", 'r')
+        self.OPENAI_ORG_ID = orgkey_file.read().rstrip()
 
-        SKLLMConfig.set_openai_key(SECRET_KEY)
-        SKLLMConfig.set_openai_org(OPENAI_ORG_ID)
+    def summarize(self, text, max=15):
+
+        wrap = [text]
+
+        SKLLMConfig.set_openai_key(self.SECRET_KEY)
+        SKLLMConfig.set_openai_org(self.OPENAI_ORG_ID)
 
         gpt_summarizer = GPTSummarizer(openai_model = "gpt-3.5-turbo", max_words = max)
-        summaries = gpt_summarizer.fit_transform(text)
-       
+        print("making api call $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        summaries = gpt_summarizer.fit_transform(wrap)
         print(summaries)
-        return summaries
+    
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    (opts, leftover) = getopt.getopt(args, "f:")
+
+    for (opt, arg) in opts:
+        if opt == "-f":
+            file = open(arg, 'r')
+            text = file.read().strip()
+            #print(text)
+            break
+    else:
+        text = leftover[0]
+
+    print(text)
+    Summarizer().summarize(text)
 
 

3c8abea28a2a85f3ecfc142cae903e57e04fbaa0
Elizabeth McDowell <mcdowell.e@northeastern.edu>
2023-07-21 11:34:31 +0100

    getting more pages

---
+++ frontend/src/components/OtherPage.tsx
@@ -0,0 +1,13 @@
+// src/components/AboutPage.tsx
+import React from 'react';
+
+const OtherPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Add content specific to the about page */}
+    </div>
+  );
+};
+
+export default OtherPage;

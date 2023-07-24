14319e11b98870d82d51013a06dbe46f91de6f43
griffin <griffinloranger@gmail.com>
2023-07-24 14:55:19 -0400

    added some new formatting for the search results
    once we get multiple results it should be in alternating colors

--- frontend/src/components/SearchPage.tsx
+++ frontend/src/components/SearchPage.tsx
@@ -3,12 +3,24 @@ import axios from 'axios';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'; // Import the Grid component
 import './SearchPage.css';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+interface SearchResult {
+  fact: string;
+  length: number;
+}
 
 const SearchPage: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [daysBack, setDaysBack] = useState('');
-  const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
 
   const handleSearch = () => {
     axios
@@ -17,7 +29,10 @@ const SearchPage: React.FC = () => {
         // Handle the response here
         console.log(response.data);
         // Extract the relevant data from the API response (this may vary depending on the API)
-        const result = response.data.fact;
+        const result: SearchResult = {
+          fact: response.data.fact,
+          length: response.data.length,
+        };
         setSearchResults([result]); // Store the response in state
       })
       .catch((error) => {
@@ -28,11 +43,19 @@ const SearchPage: React.FC = () => {
 
   return (
     <div className="SearchPage">
-      <Typography variant="h3" className="SearchHeading" style={{ marginTop: '80px', marginBottom: '20px'}}>
+      <Typography variant="h3" className="SearchHeading" style={{ marginTop: '20px' }}>
         Search Page
       </Typography>
       <div className="SearchContainer">
-        <div className="SearchBar">
+        {/* Wrap the search bar in a Box component */}
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
           <TextField
             id="outlined-basic"
             label="Enter your search here"
@@ -43,30 +66,45 @@ const SearchPage: React.FC = () => {
           <Button variant="contained" onClick={handleSearch}>
             Search
           </Button>
-        </div>
+        </Box>
         <div className="DaysBack">
-          <TextField
-          id='outlined-basic'
-          label='How many days back to look'
-          variant='outlined'
-          type='number'
+          <label>Days back to search:</label>
+          <input
+            type="number"
+            value={daysBack}
+            onChange={(e) => setDaysBack(e.target.value)}
           />
         </div>
       </div>
-            
       {/* Display the search results */}
       {searchResults.length > 0 && (
-        <div className="SearchResults">
+        <Grid container spacing={2} className="SearchResults" marginLeft={'200px'}>
+          <Grid item xs={12}>
+            <Typography variant="h4">Search Results</Typography>
+          </Grid>
           {searchResults.map((result, index) => (
-            <div key={index} className="SearchResultItem">
-              <Typography variant="body1">{result}</Typography>
-            </div>
+            <Grid item xs={8} key={index} 
+            style={{paddingBottom: '15px', backgroundColor: index % 2 === 0 ? '#f0f0f0' : '#ffffff', // Apply alternating row colors
+}}>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <Typography variant="body1">Fact:</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="body1">{result.fact}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="body1">Length:</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="body1">{result.length}</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
           ))}
-        </div>
+        </Grid>
       )}
-      
     </div>
-    
   );
 };
 

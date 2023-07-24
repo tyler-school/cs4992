05be81d3b56d48c358d86a42706d4278b435a759
Elizabeth McDowell <mcdowell.e@northeastern.edu>
2023-07-24 14:44:00 +0100

    Icon changes :)

--- frontend/src/components/EditPage.tsx
+++ frontend/src/components/EditPage.tsx
@@ -1,8 +1,10 @@
-// EditPage.tsx
+// EditPage.tsx (Updated)
 
 import React, { useState } from 'react';
+import { Grid, Button } from '@mui/material'; // Import the Grid and Button components
 import Form from './Form';
-
+import { IconButton } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 const EditPage: React.FC = () => {
   const [forms, setForms] = useState([
     { id: 1, initialSearch: 'Initial value for Form 1', initialTime: 'Time 1' },
@@ -25,19 +27,35 @@ const EditPage: React.FC = () => {
   return (
     <div>
       <h1>Multiple Forms Example</h1>
-      {forms.map((form) => (
-        <div key={form.id} style={{ marginBottom: '20px' }}>
-          <h2>Form {form.id}</h2>
-          <Form
-            initialSearch={form.initialSearch}
-            initialTime={form.initialTime}
-            onDeleteForm={() => deleteForm(form.id)}
-          />
-        </div>
-      ))}
-      <button onClick={addForm}>Add Another Form</button>
+      <Grid container spacing={2}>
+        {forms.map((form) => (
+          <Grid item key={form.id} xs={12} md={6} lg={6}>
+            <Form
+              initialSearch={form.initialSearch}
+              initialTime={form.initialTime}
+              onDeleteForm={() => deleteForm(form.id)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div style ={{ position: 'fixed', bottom: '20px', left: '20px'}}>
+        <IconButton style={{ fontSize: '50', color: 'green'}} onClick={addForm}>
+          <AddCircleIcon />
+        </IconButton>
+      </div>
+      
+
+      {/* Home button */}
+      <div style={{ position: 'fixed', bottom: '20px', right: '20px' }}>
+        <Button variant="contained" color="primary" component="a" href="/">
+          Home
+        </Button>
+      </div>
     </div>
   );
 };
 
 export default EditPage;
+
+//FAB import that make sure 
+//Import icons too

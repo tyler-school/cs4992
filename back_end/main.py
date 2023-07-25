0f6804792893f0d20eddd2782f7bb93e66554ac3
willpreebs <wpriebe16124@gmail.com>
2023-07-25 23:54:10 +0100

    NEWS-41 GET and POST for home endpoint basics implemented

--- back_end/main.py
+++ back_end/main.py
@@ -2,20 +2,19 @@ from fastapi import FastAPI
 from datetime import datetime
 from search import SearchEngine
 from article import ArticleParser
-from typing import List
 from pydantic import BaseModel
-from json import loads
+from json import loads, dumps
 
 
 app = FastAPI()
 
-class Search:
+class Search(BaseModel):
     term: str
-    start_date: str
-    end_date: str
+    days: int
 class HomePage(BaseModel):
     username: str
-    searches: List[Search]
+    searches: list[Search]
+        
 
 @app.get("/")
 def read_root():
@@ -28,22 +27,25 @@ def read_search(term, start_date, end_date, max_results=15):
     
     start_date_object = datetime.strptime(start_date, '%Y-%m-%d') # 2021-01-28
     end_date_object = datetime.strptime(end_date, '%Y-%m-%d')
-    result: List[ArticleParser] = searcher.get_news(term, start_date_object, end_date_object)
+    result: list[ArticleParser] = searcher.get_news(term, start_date_object, end_date_object)
 
     return [a.text_description() for a in result]
 
 @app.post("/home/{username}")
-def make_home_page(
-    userID: int,
-    item: HomePage):
-    home_page_file = open("home_page.json", 'rw')
-    file_contents: dict = loads(home_page_file.read())
+def make_home_page(username: str, item: HomePage):
+
+    # Create a new file
+    home_page_file = open(f"home_pages/{username}_home_page.json", 'x')
+
+    # Write homePage info to article
+    home_page_file.write(dumps(item.model_dump()))
 
     return item
 
 @app.get("/home/{username}")
-def get_home_page(userID: int):
-    read file
-    search if user exists
-    if so return home page data
-    if not error    
+def get_home_page(username: str):
+
+    home_page_file = open(f"home_pages/{username}_home_page.json", 'r')
+    page_obj = loads(home_page_file.read())
+
+    return page_obj

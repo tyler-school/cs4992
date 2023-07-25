2d272ba0ca2514abb02fb2b48334302fe3bbf810
willpreebs <wpriebe16124@gmail.com>
2023-07-25 15:15:18 +0100

    NEWS-41 start of a home page api

--- back_end/main.py
+++ back_end/main.py
@@ -3,16 +3,23 @@ from datetime import datetime
 from search import SearchEngine
 from article import ArticleParser
 from typing import List
+from pydantic import BaseModel
+from json import loads
+
 
 app = FastAPI()
+
+class Search:
+    term: str
+    start_date: str
+    end_date: str
+class HomePage(BaseModel):
+    username: str
+    searches: List[Search]
 
 @app.get("/")
 def read_root():
     return {"Root API call"}
-
-@app.get("/other")
-def read_other():
-    return {"Other API call"}
 
 @app.get("/search/{term}/{start_date}/{end_date}")
 def read_search(term, start_date, end_date, max_results=15):
@@ -25,9 +32,18 @@ def read_search(term, start_date, end_date, max_results=15):
 
     return [a.text_description() for a in result]
 
-    
+@app.post("/home/{username}")
+def make_home_page(
+    userID: int,
+    item: HomePage):
+    home_page_file = open("home_page.json", 'rw')
+    file_contents: dict = loads(home_page_file.read())
 
+    return item
 
-    
-
-    
+@app.get("/home/{username}")
+def get_home_page(userID: int):
+    read file
+    search if user exists
+    if so return home page data
+    if not error    

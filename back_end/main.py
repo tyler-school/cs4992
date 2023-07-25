0e53f9cbeabbd47e98c60d23004bcea67e3ac067
Jane Kamata <janekamata8@gmail.com>
2023-07-25 23:55:26 +0100

    Added get_home_page, which returns a list of searches based on the username in the home page file

--- back_end/main.py
+++ back_end/main.py
@@ -3,23 +3,24 @@ from datetime import datetime
 from search import SearchEngine
 from article import ArticleParser
 from typing import List
-from pydantic import BaseModel
-from json import loads
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel, ValidationError
+from json import load
 
 
 app = FastAPI()
 
-class Search:
+class Search(BaseModel):
     term: str
-    start_date: str
-    end_date: str
+    days: int
 class HomePage(BaseModel):
     username: str
     searches: List[Search]
 
 @app.get("/")
 def read_root():
-    return {"Root API call"}
+    return {"message": "Root API call"}
 
 @app.get("/search/{term}/{start_date}/{end_date}")
 def read_search(term, start_date, end_date, max_results=15):
@@ -37,13 +38,26 @@ def make_home_page(
     userID: int,
     item: HomePage):
     home_page_file = open("home_page.json", 'rw')
-    file_contents: dict = loads(home_page_file.read())
+    file_contents: dict = load(home_page_file.read())
 
     return item
 
 @app.get("/home/{username}")
-def get_home_page(userID: int):
-    read file
-    search if user exists
-    if so return home page data
-    if not error    
+def get_home_page(username: str):
+    """
+    Reads from the home page JSON. If the user exists, returns the home
+    page data as a list of searches, else errors.
+    """
+    try:
+        with open('home_page.json') as file:
+            data = load(file)
+            user_data = HomePage(**data["home_page"]) 
+
+            if user_data.username == username: 
+                return user_data.searches
+            else:
+                raise HTTPException(status_code=404, detail=f"User '{username}' not found")
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="File not found")
+    except ValidationError as ve:
+        raise HTTPException(status_code=500, detail="Error reading data: Invalid JSON format")
